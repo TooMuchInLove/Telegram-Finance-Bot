9a32d1ef0df7d1c0015a5d2993a053d214a9e3a9
Fyodorov Dmitry <fedorov.ss-4554@yandex.ru>
2024-05-17 02:14:50 +0300

    рефакторинг

--- src/services/user_data.py
+++ src/services/user_data.py
@@ -4,39 +4,37 @@ from telebot.types import User
 def get_user_name(user_data: User) -> str:
     first_name = user_data.first_name
     last_name = user_data.last_name
+    is_premium = user_data.is_premium
 
     if first_name and not last_name:
-        user_name = f"{first_name}"
+        return f"{first_name}{_get_premium_status(is_premium)}"
     elif not first_name and last_name:
-        user_name = f"{last_name}"
+        return f"{last_name}{_get_premium_status(is_premium)}"
     elif not first_name and not last_name:
-        user_name = "<user_name_empty>"
-    else:
-        user_name = f"{first_name} {last_name}"
+        return "<user_name_empty>"
 
-    if user_data.is_premium:
-        user_name = f"{user_name} ⭐"
-
-    return user_name
+    return f"{first_name} {last_name}{_get_premium_status(is_premium)}"
 
 
 def get_user_nick(user_data: User) -> str:
     user_nick = user_data.username
 
     if not user_nick:
-        user_nick = "<user_name_empty>"
-    else:
-        user_nick = f"@{user_nick}"
+        return "<user_name_empty>"
 
-    return user_nick
+    return f"@{user_nick}"
 
 
 def get_user_link(user_data: User) -> str:
     user_link = user_data.username
 
     if not user_link:
-        user_link = "#"
-    else:
-        user_link = f"https://t.me/{user_link}"
+        return "#"
 
-    return user_link
+    return f"https://t.me/{user_link}"
+
+
+def _get_premium_status(is_premium: bool) -> str:
+    if is_premium:
+        return " ⭐"
+    return ""
