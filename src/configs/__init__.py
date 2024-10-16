82b93fae40db8664011c62ae196e419b6a4ac115
Dmitry Fyodorov <fyodorov@MacBook-Air-Dmitry.local>
2024-10-16 11:02:09 +0300

    test

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -1,10 +1,12 @@
-from .env import SettingBot, setting_bot
+from .env import SettingBot, SettingsDataBase, setting_bot, setting_data_base
 from .base import config_map, DB_PATH
 from .messages import ContainerWithStaticText, LogBot
 
 __all__ = (
     "SettingBot",
+    "SettingsDataBase",
     "setting_bot",
+    "setting_data_base",
     "config_map",
     "DB_PATH",
     "ContainerWithStaticText",
--- src/configs/env.py
+++ src/configs/env.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 
 @dataclass(slots=True, frozen=True)
 class SettingBot:
+    """Settings class for Bot."""
+
     TOKEN = os_environ.get("TELEGRAM_BOT_TOKEN")
     NAME = os_environ.get("TELEGRAM_BOT_NAME")
     PROXY_URL = os_environ.get("TELEGRAM_PROXY_URL")
@@ -20,4 +22,24 @@ class SettingBot:
         return True
 
 
+@dataclass(slots=True, frozen=True)
+class SettingsDataBase:
+    """Settings class for Data Base."""
+
+    HOST = os_environ.get("DATABASE_HOST")
+    PORT = os_environ.get("DATABASE_PORT")
+    USER = os_environ.get("DATABASE_USER")
+    PASSWORD = os_environ.get("DATABASE_PASSWORD")
+    NAME = os_environ.get("DATABASE_NAME")
+    POOL_MIN_SIZE = os_environ.get("DATABASE_POOL_MIN_SIZE")
+    POOL_MAX_SIZE = os_environ.get("DATABASE_POOL_MAX_SIZE")
+    COMMAND_TIMEOUT = os_environ.get("DATABASE_COMMAND_TIMEOUT")
+
+    @property
+    def dsn(self) -> str:
+        return f"postgresql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.NAME}"
+
+
 setting_bot = SettingBot()
+
+setting_data_base = SettingsDataBase()
